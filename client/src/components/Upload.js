8e7243b52d98d794eaadcc6fe15f6ac225c72dd2
Tom Wexler <wexler18@gmail.com>
2021-05-13 20:59:23 -0400

    feed pictures added

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -8,7 +8,7 @@ const Upload = (props)=> {
     // storing the recived file from backend
     const [data, getData] = useState({ name: "", path: "" });    
     const profilePicture = useRef(); // accesing input element
-    const {setCurrentUser, currentUser, currentId } = props;
+    const {setCurrentUser, currentUser, currentId, userProfile } = props;
     const [changePic, setChangePic] = useState({
         ...currentUser,
         profilePicture:""
@@ -33,15 +33,12 @@ const Upload = (props)=> {
         formData.append('file', file); // appending file
         axios.put('http://localhost:8000/api/user/upload/' + currentId, formData, {
             withCredentials:true
-        }
-        
-        ).then(res => {
+        })
+        .then(res => {
             console.log(res.data);
-            
             getData({ name: res.data.name,
                 path: 'http://localhost:8000/' + res.data.path
             })
-
             // setChangePic({
             //     ...currentUser,
             //     profilePicture: 'http://localhost:8000/' + res.data.path
@@ -56,49 +53,64 @@ const Upload = (props)=> {
         .catch(err => console.log(err))}
 
 
-return (
+    return (
 
-<div>
+    <div>
 
-    {/* <div className="file-upload">
+        <img className="w-54 h-36 mx-auto my-2 rounded-3xl"
+        src={`http://localhost:8000/${currentUser.profilePicture}`} alt=""/>
 
-        <input ref={profilePicture} type="file" name="profilePicture"  onChange={handleInputChange} />   
+        <div className="file-upload w-56 mx-auto h-full">
 
-        {/* <div className="progessBar" style={{ width: progress }}>
-        {progress}
-        </div> */}
-        {/* <button onClick={uploadFile}>                   
-        Upload
-        </button>
-    <hr />
-    </div> */} 
+            <div className="h-10 relative w-56">
+            
+                <input className="z-0 absolute left-0 h-10 w-36 m-0 p-1 text-center placeholder-black bg-white cursor-pointer" type="text" placeholder="Change Picture" />
 
-    <div className="flex items-center justify-center bg-grey-lighter">
+                <input ref={profilePicture} 
+                type="file" name="profilePicture"  
+                onChange={handleInputChange}
+                className="opacity-0 z-50 cursor-pointer absolute left-0 h-10 w-36 m-0 p-1" />  
 
-    <label className="w-22 flex flex-col items-center 
-    px-4 pt-6 bg-white text-blue rounded-lg shadow-lg 
-    tracking-wide uppercase border border-blue cursor-pointer 
-    hover:bg-blue-200 hover:text-white">
+                <button 
+                className="p-0 m-0 w-16 absolute h-10 right-0" 
+                onClick={uploadFile}>               
+                Upload
+                </button> 
 
-        <svg className="w-8 h-8" fill="currentColor" 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 20 20">
-            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 
-            1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 
-            17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-        </svg>
+            </div>
 
-        <span className="mt-2 text-base leading-normal">Update Picture</span>
 
-        <input className="hidden"
-        ref={profilePicture} type="file" name="profilePicture"  
-        onChange={handleInputChange} />
 
-    </label>
 
-    <img onClick={uploadFile} className="w-6 mt-5 z-50" src={check} alt="" />
 
-    </div>
+    </div> 
+
+    {/* <div className="flex flex-col items-center justify-center bg-grey-lighter">
+
+        <label className="w-22 flex flex-col items-center 
+        px-4 pt-6 bg-white text-blue rounded-lg shadow-lg 
+        tracking-wide uppercase border border-blue cursor-pointer 
+        hover:bg-blue-200 hover:text-white">
+
+            <svg className="w-8 h-8" fill="currentColor" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 20 20">
+                <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 
+                1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 
+                17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+            </svg>
+
+            <span className="mt-2 text-base leading-normal">Update Picture</span>
+
+            <input className="hidden"
+            ref={profilePicture} type="file" name="profilePicture"  
+            onChange={handleInputChange} />
+
+        </label>
+
+        <img onClick={uploadFile} className="w-6 mt-2 z-50" src={check} alt="" />
+
+    </div> */}
 
 
 
